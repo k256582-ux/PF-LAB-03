46bf1611bc6d3ca2bad709ea4e34150c4eab4972
k256582-ux <k256582@nu.edu.pk>
2026-02-07 19:13:17 +0500

    Update intro.c

--- intro.c
+++ intro.c
@@ -1,3 +1,12 @@
+//Task 4
+#include <stdio.h>
+int main()
+{
+    printf("This is my first time using github");
+    return 0;
+}
+
+//task 5
 #include <stdio.h>
 
 int main()
@@ -6,3 +15,64 @@ int main()
     printf("Spring 2026\n");
     return 0;
 }
+
+//Task 6
+#include <stdio.h>
+
+int main()
+{
+    printf("Name:\tMaaniya\n");
+    printf("Roll No:\t25K-6582\n");
+    printf("Section:\tB\n");
+    return 0;
+}
+//Task 7
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    char d = 'A';
+    float b = 5.5;
+    double c = 12.34567;
+    printf("%d\n", a);
+    printf("%c\n", d);
+    printf("%f\n", b);
+    printf("%lf\n", c);
+    return 0;
+}
+//Task 8
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    char c;
+    float b;
+    
+    scanf("%d", &a);
+    scanf(" %c", &c);
+    scanf("%f", &b);
+    printf("%d %f %c", a, b, c);
+    return 0;
+}
+
+//Task  9 
+#include <stdio.h>
+
+int main() {
+    double x;
+
+    printf("Enter a number: ");
+    scanf("%lf", &num);
+
+    printf("You entered: %lf\n",num);  
+    printf("Rounded to 2 decimal places: %.2lf\n",num);
+    printf("Rounded to 5 decimal places: %.5lf\n",num);
+
+    return 0;
+}
+
+
+
+
+
